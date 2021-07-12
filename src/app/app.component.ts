8d806fc7ebc8b2803bd22ca725221db009809c97
alexander.bau <alexander.bau@nfon.com>
2021-07-12 18:25:52 +0200

    init

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,80 @@
 import { Component } from '@angular/core';
 
-@Component({
+@Component( {
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
-})
+  styleUrls: [ './app.component.scss' ]
+} )
 export class AppComponent {
-  title = 'connection-test';
+
+  public opened = false;
+  public notifications = false;
+  public history: { date: string, duration: number }[] = [];
+
+  private websocket;
+  private lastDisconnect;
+
+  constructor() {
+    this.loadHistory();
+    this.connect();
+    this.notifications = this.getNotificationPermission();
+  }
+
+  public turnOnNotificatiuons() {
+    Notification.requestPermission().then( () => {
+      this.notifications = this.getNotificationPermission();
+    } );
+  }
+
+  public getNotificationPermission() {
+    return Notification.permission !== 'denied' && Notification.permission !== 'default' && localStorage.notifications !== 'off';
+  }
+
+  public changeNotificationToggle( $event ) {
+    localStorage.notifications = $event.checked ? 'on' : 'off';
+    this.notifications = this.getNotificationPermission();
+  }
+
+  private connect() {
+    this.websocket = new WebSocket( 'wss://echo.websocket.org' );
+    this.websocket.onopen = () => this.onOpen();
+    this.websocket.onclose = () => this.onClose();
+    this.websocket.onmessage = ( ...args ) => console.log( args );
+    this.websocket.onerror = ( ...args ) => console.log( args );
+  }
+
+  private onOpen() {
+    this.opened = true;
+    if ( this.lastDisconnect ) {
+      this.history.unshift( {
+        date: ( new Date( this.lastDisconnect ) ).toString(),
+        duration: Date.now() - this.lastDisconnect
+      } );
+      this.lastDisconnect = undefined;
+      this.saveHistory();
+    }
+  }
+
+  private onClose() {
+    this.opened = false;
+    if ( !this.lastDisconnect ) {
+      this.lastDisconnect = Date.now();
+      if ( this.notifications ) {
+        new Notification( 'Disconnected' );
+      }
+    }
+    setTimeout( () => {
+      this.connect();
+    }, 1000 );
+  }
+
+  private saveHistory() {
+    localStorage.history = JSON.stringify( this.history, null, 0 );
+  }
+
+  private loadHistory() {
+    if ( localStorage.history ) {
+      this.history = JSON.parse( localStorage.history );
+    }
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,28 @@
-import { NgModule } from '@angular/core';
+import { LOCALE_ID, NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatSlideToggleModule } from '@angular/material/slide-toggle';
+import { MatListModule } from '@angular/material/list';
+import localeDe from '@angular/common/locales/de';
+import { registerLocaleData } from '@angular/common';
 
-@NgModule({
+registerLocaleData( localeDe );
+
+@NgModule( {
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    MatToolbarModule,
+    MatSlideToggleModule,
+    MatListModule
   ],
-  providers: [],
-  bootstrap: [AppComponent]
-})
+  providers: [ { provide: LOCALE_ID, useValue: "de" } ],
+  bootstrap: [ AppComponent ]
+} )
 export class AppModule { }
